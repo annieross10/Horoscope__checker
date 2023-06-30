8b1a087c07bea65b5317d50cb7644f25bf006d5b
Annie Ross <annieross10@outlook.com>
2023-06-30 23:10:11 +0100

    add notes for game structure

---
+++ run.py
@@ -0,0 +1,20 @@
+import random
+
+
+#Horoscopes
+
+#Days of the month
+
+#Star signs
+
+#Predictions for either today or tomorrow
+
+#Get birthdate information
+
+#Calculate star sign
+
+#Ask if user wants to know horoscope
+
+#Horoscope prediction for either today or tomorrow
+
+#Horoscope prediciton
